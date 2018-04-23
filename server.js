0c2ee61789024d86204c8508c1d6c3b89bb8b15b
John Cokos <highlander.44@gmail.com>
2018-04-23 16:29:33 -0700

    It might live?

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const cors = require('cors');
+const pg = require('pg');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('wat'));
+
+app.get('/api/v1/tasks', (req, res) => {
+  client.query(`SELECT * from tasks;`)
+  .then(results => res.send(results.rows))
+  .catch(console.error);
+});
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+
